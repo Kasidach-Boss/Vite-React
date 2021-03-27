f56a1d67bcf34d7c2697b9a380fc130ccaa79709
Kasidach-Boss <kasidachst@gmail.com>
2021-03-28 00:39:20 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { BrowserRouter as Router,  Switch,  Route,  Link} from 'react-router-dom';
+import { Navbar,Nav,NavDropdown,Form,FormControl,Button,Toast } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import ToastHeader from 'react-bootstrap/ToastHeader'
+import ToastBody from 'react-bootstrap/ToastBody'
+import WordArt from 'react-wordart';
+import Hello from './page/Hello';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [show, setShow] = useState(false)
+  const mystyle = {
+    color: "white",
+    padding: "10px",
+    fontFamily: "Arial"
+  };
+  return (
+    <Router >
+     
+      <Navbar bg="primary" variant="dark">
+      <Navbar.Brand href="#home" style={mystyle}><WordArt text='My React App' theme={`rainbow`} fontSize={20}></WordArt> </Navbar.Brand>
+          <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/About">About</Nav.Link>
+              <Nav.Link href="/Dashboard">Dashbaord</Nav.Link>
+              <Nav.Link href="/Hello">Hello</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          </Form>
+          <Button variant="outline-light">Search</Button>
+
+        
+        </Navbar>
+        
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/Hello">
+            <Hello/>
+          </Route>
+         
+        </Switch>
+      
+    </Router>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+      
+    
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+export default App
---
+++ src/page/Hello.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import { Card, Container } from 'react-bootstrap';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button'
+import WordArt from 'react-wordart'
+
+function Hello(){
+    const [count,setCount] = useState(0);
+    const diviedtwo = () =>{
+        setCount((count)=> count/2)
+       
+    }
+    const Power =() =>{
+        setCount((count)=> count*count)
+        
+    }
+    console.log(count);
+    return (
+        <Jumbotron fluid>
+            <Container>
+                <br></br>
+                <center><WordArt text='Hello, Vite+React!!!!!!' theme={`slate`} fontSize={50}></WordArt></center>
+                <br></br>
+                <Card>
+                 <Card.Body><h2>count: {count}</h2>   </Card.Body> 
+                </Card>
+                <br></br>
+                <Button variant="primary" onClick={()=>setCount((count)=> count+1)}>Increase</Button>
+                <Button  variant="info" onClick={()=>setCount((count)=> count-1)}>Decrease</Button>
+                <Button variant="danger" onClick={diviedtwo}>Divied2</Button>
+                <Button variant="success" onClick={()=>setCount((count)=> count=0)}>Reset</Button>
+                <Button variant="secondary" onClick={Power}>Power</Button>
+            </Container>
+                     
+        </Jumbotron>
+        
+        
+    )
+   
+}
+
+export default Hello
